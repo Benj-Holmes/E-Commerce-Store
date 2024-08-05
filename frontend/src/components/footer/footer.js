3b36377b0eef754b0ab7fd63ce3b9a537952715d
Benjamin Holmes <HolmesBen@Proton.me>
2024-08-06 00:50:08 +0100

    Main Page Styling Completed

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './footer.css';
+import github from './github.png';
 
 const Footer = () => {
     return (
-        <div>
-            hi!
+        <div className='footerContainer'>
+            <div className='footDiv'>
+                <ul>
+                    <li className='listTitle'> Visit Us:</li>
+                    <li> 123 Fake Street </li>
+                    <li> Nowhereville  </li>
+                    <li> AA10 1AB </li>
+                </ul>
+            </div>
+            <div className='footDiv'>
+                <ul>
+                    <li className='listTitle'> Contact Us: </li>
+                    <li> Sales@OneBillionClothing.co.uk </li>
+                    <li> Accounts@OneBillionClothing.co.uk </li>
+                    <li> 029 2064 9200 </li>
+                </ul>
+            </div>
+            <div className='footDiv'>
+            </div>
+            <div className='github'>
+                <a href='https://github.com/Benj-Holmes/E-Commerce-Store' target='_blank'>
+                    <p className='githubText'> Visit this Project on Github! </p>
+                    <img src={github} alt='Link to this Projects Github Repository' />
+                </a>
+            </div>
         </div>
     );
 }
--- frontend/src/components/mainPage/productSpotlight.js
+++ frontend/src/components/mainPage/productSpotlight.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import './mainPage.css';
+import shoe1 from '../../imgs/productSpotlightImgs/prod1.jpg';
+import shoe2 from '../../imgs/productSpotlightImgs/prod2.jpg';
+import shoe3 from '../../imgs/productSpotlightImgs/prod3.jpg';
+import shoe4 from '../../imgs/productSpotlightImgs/prod4.jpg';
 
 const ProductSpotlight = () => {
     return (
-        <div>
-            Hi!
+        <div class='spotlight'>
+            <div className='spotlightHeader'>
+                <h3> A NEW RANGE OF TRAINERS - <span> COMING SOON </span> </h3>
+            </div>
+            <div className='spotlightContainer'>
+                <div className='productSpotlight'>
+                    <img src={shoe2} />
+                    <div className="overlay">
+                        <figcaption className='upperText'> Nike Air Force 1 '07 LV8 UT </figcaption>
+                        <figcaption className='lowerText'> £115.00 </figcaption>
+                    </div>
+                </div>
+                <div className='productSpotlight'>
+                    <img src={shoe1} />
+                    <div className="overlay">
+                        <figcaption className='upperText'> Nike React Pegasus Trail 4 </figcaption>
+                        <figcaption className='lowerText'> £145.00 </figcaption>
+                    </div>
+                </div>
+                <div className='productSpotlight'>
+                    <img src={shoe4} />
+                    <div className="overlay">
+                        <figcaption className='upperText'> Nike Downshifter 12 </figcaption>
+                        <figcaption className='lowerText'> £60.00 </figcaption>
+                    </div>
+                </div>
+                <div className='productSpotlight'>
+                    <img src={shoe3} />
+                    <div className="overlay">
+                        <figcaption className='upperText'> Nike Air Max Alpha TR 4 </figcaption>
+                        <figcaption className='lowerText'> £73.00 </figcaption>
+                    </div>
+                </div>
+            </div>
+            <div className='spotlightHeader'>
+                <h4> SIGN UP TO THE MAILING LIST TO PRE-ORDER </h4>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         </div>
     );
 }
