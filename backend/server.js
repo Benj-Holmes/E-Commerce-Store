1703030ccc7effb2121e97e03be912e499f18d03
Benjamin Holmes <HolmesBen@Proton.me>
2024-08-05 22:04:25 +0100

    Tested Auth, Login, Logout and Cookie

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,69 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-// require('./config/passportconfig');
-// require('./config/googlepassport');
+require('./config/googlePassport');
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-// app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+// Middleware ---------------------------------------------------------------------
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
-// Route Handlers -------------------------------------------
-// app.use('/cart', require('./routes/cartRoutes'));
-// app.use('/orders', require('./routes/orderRoutes'));
-// app.use('/payment', require('./routes/paymentRoutes'));
-// app.use('/products', require('./routes/productRoutes'));
-// app.use('/reviews', require('./routes/reviewRoutes'));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    preflightContinue: true
+}));
+
+const store = new session.MemoryStore();
+app.use(session({ 
+    secret: process.env.SESSION_SECRET, 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+        sameSite: false,
+        secure: false,
+        maxAge: 300000000000
+    },
+    store 
+}));
+
+// Passport Config --------------------------------------------------------------------
+
+require('./config/passportConfig');
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Log In
+app.post('/auth/login', passport.authenticate('local'), (req, res, next) => {
+    res.send(req.user);
+});
+
+// Log Out 
+app.get('/auth/logout', (req, res) => {
+    req.logOut(() => {
+        res.send({ message: 'Logged Out Successfully!'});
+    })
+})
+
+// Google Login -----------------------------------------------------------------------
+require('./config/googlePassport');
+app.get('/auth/google', passport.authenticate('google', {
+    scope: 'profile',
+}), )
+
+app.get('/auth/google/callback', passport.authenticate("google", { session: true }), (req, res) => {
+    // Cant Res.send and res.redirect, so i think i have to call getUser after this.
+    // res.send(req.user);
+    res.redirect('http://localhost:3000/account')
+})
+
+// Route Handlers ---------------------------------------------------------------------
+app.use('/api/cart', require('./routes/cartRoutes'));
+app.use('/api/orders', require('./routes/orderRoutes'));
+app.use('/api/payment', require('./routes/paymentRoutes'));
+app.use('/api/product', require('./routes/productRoutes'));
+app.use('/api/reviews', require('./routes/reviewRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
 
 // Start server
