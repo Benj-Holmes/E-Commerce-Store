b9e372d7638cec3992522ff2f8d153ec1c8c0651
Benjamin Holmes <HolmesBen@Proton.me>
2024-08-06 00:01:42 +0100

    Image Carousel added

--- frontend/src/components/mainPage/seasonalDisplay.js
+++ frontend/src/components/mainPage/seasonalDisplay.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import './mainPage.css'
 
 const SeasonalDisplay = () => {
+    const [imgNo, setImgNo] = useState(1);
+    const imgSrc = `/display/${imgNo}.jpg`;
+
+    useEffect(() => {
+        const updateImg = () => {
+            setImgNo(prevImgNo => {
+              if (prevImgNo === 1) return 2;
+              if (prevImgNo === 2) return 3;
+              return 1; 
+            });
+        }
+        const setIntervalImg = setInterval(updateImg, 8000);
+        return () => clearInterval(setIntervalImg);    
+
+    }, []);
+
     return (
-        <div>
-            Hi!
+        <div className='seasonalDisplay'>
+            <h1> Shop Our New Seasonal Ranges </h1>
+            <img src={imgSrc} className='background' alt='A Seasonal Clothing Display' />
         </div>
     );
 }
