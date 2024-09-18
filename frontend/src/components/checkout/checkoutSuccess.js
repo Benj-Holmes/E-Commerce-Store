a9f88090b3cfb35ccd2c052550efbe2f1ff25a46
Benjamin Holmes <HolmesBen@Proton.me>
2024-09-18 10:26:02 +0100

    Added CSS and Redirect to 'Checkout Success' Page

--- frontend/src/components/checkout/checkoutSuccess.js
+++ frontend/src/components/checkout/checkoutSuccess.js
@@ -4,25 +4,50 @@ import { useDispatch } from 'react-redux';
 import { getUser } from '../../slices/authSlice';
 import { createOrder } from '../../slices/accountPageSlice';
 import { setCartToInactive } from '../../slices/cartSlice';
+import logo from '../../imgs/BillionLogo.png';
+import { useNavigate } from 'react-router-dom';
 
 const CheckoutSuccess = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(getUser());
         dispatch(createOrder());
         dispatch(setCartToInactive());
         
+        const reDirect = setTimeout(() => {
+            navigate('/');
+        }, 10000)
+
+        return () => clearTimeout(reDirect);
     });
 
     return (
+        <>
         <div className='successBox'>
+            <img src={'/display/AccountBackground.png'} />
             <h1> Your Checkout was Succesful! </h1>
+        </div>
+        <div className='successText'>
             <p> Thank you for your business, we hope that you found everything you were looking for
                 on our Site. If you have any feedback, please don't hesitate to get in touch, our contact
-                details can be found in the Footer of our website.
+                details can be found in the Footer below.
             </p> 
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <img src={logo} className='successLogo'/>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <p> You will be Redirected to the Home Page in 10 Seconds.</p>
         </div>
+        </>
     );
 }
 
